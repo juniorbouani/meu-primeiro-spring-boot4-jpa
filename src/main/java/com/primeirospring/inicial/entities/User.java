21e398b65db767068feababa2cac6bc101699201
juniorbouani <juniorbouani@gmail.com>
2026-04-22 21:27:11 -0300

    JPA repository

--- src/main/java/com/primeirospring/inicial/entities/User.java
+++ src/main/java/com/primeirospring/inicial/entities/User.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
-@Table (name = "tb_user")
+@Table (name = "user")
 public class User implements Serializable {
 
 
---
+++ src/main/java/com/primeirospring/inicial/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.primeirospring.inicial.repositories;
+
+import com.primeirospring.inicial.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
