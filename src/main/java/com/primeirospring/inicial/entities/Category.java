cd3e13aced1b5ad18bfb925ef268aa97f21a05ff
juniorbouani <juniorbouani@gmail.com>
2026-04-25 22:26:26 -0300

    Linkando categorias com produtos

--- src/main/java/com/primeirospring/inicial/entities/Category.java
+++ src/main/java/com/primeirospring/inicial/entities/Category.java
@@ -1,5 +1,6 @@
 package com.primeirospring.inicial.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -19,7 +20,8 @@ public class Category implements Serializable {
     private Long Id;
     private String name;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
