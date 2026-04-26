b4c35c17193fb9140feabb6b1eca08b2962bf6f8
juniorbouani <juniorbouani@gmail.com>
2026-04-26 13:30:06 -0300

    Tratando excessao do update

--- src/main/java/com/primeirospring/inicial/services/UserService.java
+++ src/main/java/com/primeirospring/inicial/services/UserService.java
@@ -4,6 +4,7 @@ import com.primeirospring.inicial.entities.User;
 import com.primeirospring.inicial.repositories.UserRepository;
 import com.primeirospring.inicial.services.exceptions.DatabaseException;
 import com.primeirospring.inicial.services.exceptions.ResourceNotFoundExcepiton;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -48,9 +49,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch ( EntityNotFoundException e) {
+            throw new ResourceNotFoundExcepiton(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
